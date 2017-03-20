3e4e935ad62c60d3d17107a42500ef9893d2b94e
Kenny Groszman <kennygrosz@gmail.com>
2017-03-20 15:18:59 -0500

    updated code and Project Proposal.docx

--- code/blackjacksim.m
+++ code/blackjacksim.m
@@ -1,4 +1,4 @@
-function [cards, opt_strat, opt_pay] = blackjacksim(p,decks)
+function [cards, dealer_sum, opt_strat, opt_pay] = blackjacksim(p,decks)
 %play blackjack for p players and one dealer with the specified number of
 %decks
 
@@ -28,21 +28,36 @@ end
 %-------play as player------------------
 
 %---stand---%
-pay(1) = payout(player, dealer);
+if bj_sum(player) <= 11
+    pay(1) = -inf;
+else
+    pay(1) = payout(player, dealer);
+end
+
 
 %---hit once---
 hit1 = deckdraw(1,C,decks); %draw one card
 playerh1 = [player, hit1]; %add hit to player's cards
 C = [C, hit1]; %add the hit card to the cards in play
 
-pay(2) = payout(playerh1, dealer);
+if bj_sum(playerh1) <= 11
+    pay(2) = -inf;
+else
+    pay(2) = payout(playerh1, dealer);
+end
+
 
 %---hit a second time---
 hit2 = deckdraw(1,C,decks); %draw one card
 playerh2 = [playerh1, hit2]; %add hit to player's cards
 C = [C, hit2]; %add the hit card to the cards in play
 
-pay(3) = payout(playerh2, dealer);
+if bj_sum(playerh2) <= 11
+    pay(3) = -inf;
+else
+    pay(3) = payout(playerh2, dealer);
+end
+
 
 %---hit a third time---
 hit3 = deckdraw(1,C,decks); %draw one card
@@ -51,30 +66,31 @@ C = [C, hit3]; %add the hit card to the cards in play
 
 pay(4) = payout(playerh3, dealer);
 
-%---split---
-if player(1) ~= player(2)
-    pay(5) =  -inf; %in situations where you can't split, make the payout infinitely negative
-else
-    %you can only split if it's the same card twice
-
-    player1 = [player(1), hit1]; %new hand one
-    player2 = [player(2), hit2]; %new hand two
-
-    pay(5) = payout(player1, dealer) + payout(player2,dealer); %payout of both hands
-end
-
-%--- double down ---
-    %take the player who hit once and double his payout
-    pay(6) = 2* payout(playerh1, dealer);
-    %**********************************************************
-    %NOTE: THIS STRATEGY WILL ALWAYS BE PREFERRED TO HITTING ONCE, SO THIS
-    %IS WRONG
-    %**********************************************************
+% %---split---
+% if player(1) ~= player(2)
+%     pay(5) =  -inf; %in situations where you can't split, make the payout infinitely negative
+% else
+%     %you can only split if it's the same card twice
+% 
+%     player1 = [player(1), hit1]; %new hand one
+%     player2 = [player(2), hit2]; %new hand two
+% 
+%     pay(5) = payout(player1, dealer) + payout(player2,dealer); %payout of both hands
+% end
+% 
+% %--- double down ---
+%     %take the player who hit once and double his payout
+%     pay(6) = 2* payout(playerh1, dealer);
+%     %**********************************************************
+%     %NOTE: THIS STRATEGY WILL ALWAYS BE PREFERRED TO HITTING ONCE, SO THIS
+%     %IS WRONG
+%     %**********************************************************
 
     
 % generate outputs to function
 
-cards = [dealer(1), player(1), player(2)];
+dealer_sum = d_sum;
+cards = [dealer(1), dealer(2), player(1), player(2),hit1,hit2,hit3];
 
 opt_strat = find(pay==max(pay),1); %***** NEED TO INCORPORATE HOW TO DEAL WITH TIES. Right now, just takes the first answer (in theory, the least risky??)
 opt_pay = max(pay);
